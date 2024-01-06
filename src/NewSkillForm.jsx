fe2465b7bda6c38cacbbaa98f798842562210279
phillaung <yellowpete.kog@gmail.com>
2024-01-05 22:26:30 -0800

    part one

---
+++ src/NewSkillForm.jsx
@@ -0,0 +1,17 @@
+export default function NewSkillForm() {
+    return (
+        <form>
+            <label>Skill</label>
+            <input type='text' />
+            <label>Level</label>
+            <select>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+            </select>
+            <button>ADD SKILL</button>
+        </form>
+    );
+}
