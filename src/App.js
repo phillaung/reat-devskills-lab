7a356cbc78b1dce5785b1cd945c1dfb73b5cc5ce
phillaung <yellowpete.kog@gmail.com>
2024-01-09 23:01:39 -0800

    part three using state for skills array

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
 import SkillList from './SkillList';
 import NewSkillForm from './NewSkillForm';
+import { useState } from 'react';
 
 function App() {
-  const skills = [
+  const [skills, setSkills] = useState([
       { name: 'HTML', level: 5 },
       { name: 'CSS', level: 3 },
       { name: 'JavaScript', level: 4 },
       { name: 'Python', level: 2 },
-  ];
+  ]);
   return (
-    <div className="App">
-      <h1>React Dev Skills</h1>
-      <SkillList skills={skills}/>
-      <hr/>
-      <NewSkillForm />
-    </div>
+      <div className='App'>
+          <h1>React Dev Skills</h1>
+          {skills && <SkillList skills={skills} />}
+          <hr />
+          <NewSkillForm />
+      </div>
   );
 }
 
