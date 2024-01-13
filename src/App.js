a9b076e4336451f7b1611b0d2e978e2551f2b40f
phillaung <yellowpete.kog@gmail.com>
2024-01-12 21:20:40 -0800

    part 4; submit form to add new skill using event handler

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ function App() {
       { name: 'JavaScript', level: 4 },
       { name: 'Python', level: 2 },
   ]);
+  function addNewSkill(skill) {
+    const newSkills = [...skills, skill]
+    setSkills(newSkills)
+  }
   return (
       <div className='App'>
           <h1>React Dev Skills</h1>
           {skills && <SkillList skills={skills} />}
           <hr />
-          <NewSkillForm />
+          <NewSkillForm addNewSkill={addNewSkill} />
       </div>
   );
 }
--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './NewSkillForm.css'
-export default function NewSkillForm() {
+export default function NewSkillForm({ addNewSkill}) {
+    const [formData, setFormData] = useState({
+        name: '',
+        level: '1',
+    })
+    function handleChange(evt) {
+        const newFormData = { ...formData, [evt.target.name]: evt.target.value }
+        setFormData(newFormData)
+    }
+    function handleAddSkill(evt) {
+        evt.preventDefault()
+        addNewSkill(formData)
+    }
     return (
-        <form>
+        <form onSubmit={handleAddSkill}>
             <label>Skill</label>
-            <input type='text' />
+            <input name='name' type='text' onChange={handleChange} />
             <label>Level</label>
-            <select>
+            <select name='level' onChange={handleChange}>
                 <option value='1'>1</option>
                 <option value='2'>2</option>
                 <option value='3'>3</option>
